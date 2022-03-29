e7e91efd817a41877b46a8083181ad3886e26566
alirezaabdollahpour <alirezaabdollahpour1380@gmail.com>
2022-03-30 01:51:38 +0430

    Node copy constructor

--- src/bst.cpp
+++ src/bst.cpp
@@ -6,3 +6,10 @@ BST::Node::Node(int value, Node* left, Node* right)
     this->right = right;
     this->value = value;
 }
+
+BST::Node::Node(const Node& node) // copy Node constructor
+{
+    value = node.value;
+    right = node.right;
+    left = node.left;
+}
