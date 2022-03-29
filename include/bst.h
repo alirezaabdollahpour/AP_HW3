ce8599439503614bc2952a08743e5d44e408b03e
alirezaabdollahpour <alirezaabdollahpour1380@gmail.com>
2022-03-30 01:09:54 +0430

    addes Node class in BST class

---
+++ include/bst.h
@@ -0,0 +1,33 @@
+#ifndef BST_H
+#define BST_H
+
+#include <functional>
+#include <iostream>
+#include <string>
+#include <vector>
+class BST {
+public:
+    class Node {
+    public:
+        Node(int value, Node* left, Node* right);
+        Node(); // copy constructor
+        Node(const Node& node);
+
+        int value;
+        Node* left;
+        Node* right;
+    };
+    Node*& get_root();
+    void bfs(std::function<void(Node*& node)> func);
+    size_t length();
+    bool add_node(int value);
+    Node** find_node(int value);
+    Node** find_parrent(int value);
+    Node** find_successor(int value);
+    bool delete_node(int value);
+
+private:
+    Node* root;
+};
+
+#endif // BST_H
