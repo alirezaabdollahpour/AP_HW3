dd2232092e646e715ae058f25e580765a6fc120c
alirezaabdollahpour <alirezaabdollahpour1380@gmail.com>
2022-04-01 22:25:51 +0430

    add node func is implement

--- src/bst.cpp
+++ src/bst.cpp
@@ -17,3 +17,42 @@ BST::Node*& BST::get_root()
 {
     return root;
 }
+
+bool BST::add_node(int value)
+{
+    if (get_root() == nullptr) {
+        root = new Node;
+        root->value = value;
+        root->right = nullptr;
+        root->left = nullptr;
+        return 1;
+    }
+    BST::Node* pntr { root };
+    while (1) {
+        if (value > pntr->value) {
+            if (pntr->right == nullptr) {
+                pntr->right = new Node;
+                pntr = pntr->right;
+
+                pntr->value = value;
+                std::cout << *(pntr) << std::endl;
+                return 1;
+            } else {
+                pntr = pntr->right;
+            }
+        } else {
+            if (value == pntr->value)
+                return 0;
+            if (pntr->left == nullptr) {
+                pntr->left = new Node;
+                pntr = pntr->left;
+
+                pntr->value = value;
+                std::cout << *(pntr) << std::endl;
+                return 1;
+            } else {
+                pntr = pntr->left;
+            }
+        }
+    }
+}
