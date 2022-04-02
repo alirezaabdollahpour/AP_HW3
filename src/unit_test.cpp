348f6c2a30c92ba937398026bae13abd460069ec
alirezaabdollahpour <alirezaabdollahpour1380@gmail.com>
2022-04-03 01:41:44 +0430

    15 test passed

--- src/unit_test.cpp
+++ src/unit_test.cpp
@@ -141,7 +141,7 @@ TEST(HW3Test, TEST10)
 
     EXPECT_EQ(bst.length(), 6);
 }
-/*
+
 TEST(HW3Test, TEST11)
 {
     BST bst {};
@@ -156,8 +156,9 @@ TEST(HW3Test, TEST11)
     std::cout << bst << std::endl;
 }
 
-TEST(HW3Test, TEST12) {
-    BST bst{};
+TEST(HW3Test, TEST12)
+{
+    BST bst {};
     bst.add_node(25);
     bst.add_node(10);
     bst.add_node(50);
@@ -165,14 +166,15 @@ TEST(HW3Test, TEST12) {
     bst.add_node(15);
     bst.add_node(7);
 
-    BST::Node** node{bst.find_node(10)};
+    BST::Node** node { bst.find_node(10) };
     EXPECT_EQ((*node)->value, 10);
     EXPECT_EQ((*node)->left->value, 7);
     EXPECT_EQ((*node)->right->value, 15);
 }
 
-TEST(HW3Test, TEST13) {
-    BST bst{};
+TEST(HW3Test, TEST13)
+{
+    BST bst {};
     bst.add_node(25);
     bst.add_node(10);
     bst.add_node(50);
@@ -180,12 +182,13 @@ TEST(HW3Test, TEST13) {
     bst.add_node(15);
     bst.add_node(7);
 
-    BST::Node** node{bst.find_node(11)};
+    BST::Node** node { bst.find_node(11) };
     EXPECT_EQ(node, nullptr);
 }
 
-TEST(HW3Test, TEST14) {
-    BST bst{};
+TEST(HW3Test, TEST14)
+{
+    BST bst {};
     bst.add_node(25);
     bst.add_node(10);
     bst.add_node(50);
@@ -194,12 +197,14 @@ TEST(HW3Test, TEST14) {
     bst.add_node(7);
 
     std::cout << "here" << std::endl;
-    BST::Node** node{bst.find_parrent(15)};
+    BST::Node** node { bst.find_parrent(15) };
     std::cout << "here2" << std::endl;
     EXPECT_EQ((*node)->value, 10);
     EXPECT_EQ((*node)->left->value, 7);
     EXPECT_EQ((*node)->right->value, 15);
 }
+
+/*
 
 TEST(HW3Test, TEST15) {
     BST bst{};
