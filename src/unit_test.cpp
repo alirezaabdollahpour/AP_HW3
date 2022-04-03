f1646ec9a21fdc85b4db506870b17a4bb9551d31
alirezaabdollahpour <alirezaabdollahpour1380@gmail.com>
2022-04-03 16:50:33 +0430

    all test pass seccessfully :)))))

--- src/unit_test.cpp
+++ src/unit_test.cpp
@@ -483,37 +483,38 @@ TEST(HW3Test, TEST28)
     EXPECT_EQ(adddress_of_root, move.get_root());
     EXPECT_EQ(test_value, move.get_root()->left->right->value);
 }
-/*
-TEST(HW3Test, TEST29) {
-    BST bst1{5, 1, 10, 2, 8, 50, 4, 60};
-    BST bst2{3, 2, 100, 20, 8, 50, 4, 60, 44, 23};
+
+TEST(HW3Test, TEST29)
+{
+    BST bst1 { 5, 1, 10, 2, 8, 50, 4, 60 };
+    BST bst2 { 3, 2, 100, 20, 8, 50, 4, 60, 44, 23 };
     EXPECT_EQ((*bst1.find_successor(5))->value, 4);
     EXPECT_EQ(bst2.length(), 10);
 }
 
-TEST(HW3Test, TEST30) {
-    BST bst{5, 1, 10, 2, 8, 50, 4, 60};
-    BST bst2{++bst};
+TEST(HW3Test, TEST30)
+{
+    BST bst { 5, 1, 10, 2, 8, 50, 4, 60 };
+    BST bst2 { ++bst };
 
     std::vector<int> values;
     EXPECT_EQ(bst.get_root()->value, 6);
     EXPECT_EQ(bst2.get_root()->value, 6);
     EXPECT_EQ(bst.get_root()->right->right->right->value, 61);
     EXPECT_EQ(bst2.get_root()->right->right->right->value, 61);
-
 }
 
-TEST(HW3Test, TEST31) {
-    BST bst1{5, 1, 10, 2, 8, 50, 4, 60};
-    BST bst2{bst1++};
+TEST(HW3Test, TEST31)
+{
+    BST bst1 { 5, 1, 10, 2, 8, 50, 4, 60 };
+    BST bst2 { bst1++ };
 
     std::vector<int> values1;
-    bst1.bfs([&values1](BST::Node*& node){values1.push_back(node->value);});
+    bst1.bfs([&values1](BST::Node*& node) { values1.push_back(node->value); });
 
     std::vector<int> values2;
-    bst2.bfs([&values2](BST::Node*& node){values2.push_back(node->value);});
+    bst2.bfs([&values2](BST::Node*& node) { values2.push_back(node->value); });
 
-    for(size_t i{}; i < values1.size(); i++)
-        EXPECT_EQ(values2[i], values1[i]-1);
+    for (size_t i {}; i < values1.size(); i++)
+        EXPECT_EQ(values2[i], values1[i] - 1);
 }
-*/
